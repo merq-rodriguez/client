dbc826827d5a18bdf4a0b0ce1c6c1dd8655dc8ec
Stiven Rodriguez <merq.rodriguez@udla.edu.co>
2019-02-28 18:11:59 -0500

    fix body signup and validate user

--- src/scenes/WelcomeSignup/index.js
+++ src/scenes/WelcomeSignup/index.js
@@ -6,12 +6,61 @@ import './styles.scss';
 
 class WelcomeSignup extends React.Component {
 
-  state = {  };
+  state = { 
+    err: false,
+    message: '',
+    user: {}
+  };
+
+  emailRef =        React.createRef();
+  emialConfirmRef = React.createRef();
+  nameRef =         React.createRef();
+  passwordRef =     React.createRef();
+
+
+  validateEmptyField(){
+    if(this.state.user.email === '' || this.state.user.name === '' || this.state.user.password === '' ){
+      this.setState({ err: true,  message: '* Ingrese los campos obligatorios' })
+    }else{
+      this.setState({ err: true,  message: ''})
+    }
+  }
+
+  signUp = (e) => {
+    e.preventDefault();
+
+    const dataUser = {
+      email: this.emailRef.current.value,
+      name: this.nameRef.current.value,
+      password: this.passwordRef.current.value
+    }
+
+    this.setState({
+       data: dataUser 
+    })
+
+    this.validateEmptyField();
+
+/* 
+    if(dataUser.email === '' || dataUser.name === '' || dataUser.password === '' ){
+      this.setState({
+         err: true, 
+         message: 'Ingrese los campos obligatorios'
+      })
+    }else{
+      if(dataUser.email !== this.emialConfirmRef.current.value){
+        console.log("Verifique que coincida el email")
+        this.setState({ err: true })
+      }
+      this.setState({ err: false })
+    } */
+    console.log(dataUser);
+  }
 
   render() {
   return (
     
-    <div>
+    <div  className="container-signup"> 
        <header>
         <img src={pufy}/> 
       </header>
@@ -19,17 +68,18 @@ class WelcomeSignup extends React.Component {
      
     <section>
         <h3>Registra tu dirección de email</h3>
-        <form>
-            <input type="email" placeholder="Email" name=""/>
-            <input type="email" placeholder="Confirmar email" name=""/>
-            <input type="text" placeholder="Nombre" name=""/>
-            <input type="password" placeholder="Contraseña" name=""/>
+        <form onSubmit={this.signUp}>
+            <input type="email"  placeholder="Email" name="" ref={this.emailRef} />
+            <p> * Campos obligatorios</p>
+            <input type="email" placeholder="Confirmar email" ref={this.emialConfirmRef} name=""/>
+            <input type="text" placeholder="Nombre"  ref={this.nameRef} name=""/>
+            <input type="password" placeholder="Contraseña" ref={this.passwordRef} name=""/>
 
             <div className="rd">
-
+            { this.state.err ? <p> Error </p> : '' }
                 <input type="radio" value="" id="rd" name=""/>
 
-                <label for="rd">
+                <label>
                     Compatir mis datos de registro con los proveedores de contenido de Pufy para fines de Marketing
                 </label>
 
